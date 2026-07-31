7d69151fb409ddaa6a8b7d1cfec511e42219f7eb
dvviii857-pixel <dvviii857@gmail.com>
2026-07-31 10:52:05 +0800

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-/**
- * Konhill article JavaScript
- * The current page is mostly static and does not require JavaScript.
- * Add future interactive behavior in this file.
- */
-document.documentElement.classList.add('js');

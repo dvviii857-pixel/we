1efb1551c486adebca3a540fd9cdd286ae5ca80d
dvviii857-pixel <dvviii857@gmail.com>
2026-07-31 11:09:26 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Konhill article JavaScript
+ * The current page is mostly static and does not require JavaScript.
+ * Add future interactive behavior in this file.
+ */
+document.documentElement.classList.add('js');
